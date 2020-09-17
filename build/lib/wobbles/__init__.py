34d4890f0ba909fdbba7bf7266b26013f16f311b
Morgan Bennett <morgeanb.phys+GitHub@gmail.com>
2020-09-17 10:51:09 -0400

    Remove unnecessary files

--- build/lib/wobbles/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Top-level package for wobbles."""
-
-__author__ = """Morgan Jane Bennett"""
-__email__ = 'bennett@astro.utoronto.ca'
-__version__ = '0.1.0'
-
-from wobbles import *
--- docs/usage.rst
+++
@@ -1,7 +0,0 @@
-=====
-Usage
-=====
-
-To use wobbles in a project::
-
-    import wobbles
