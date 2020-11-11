e4cb4cd8b2d5693d1c630e14c2ee89aa446de613
Daniel Gilman <gilmanda@ucla.edu>
2020-11-11 00:07:26 -0500

    edit forward model script

--- wobbles/workflow/forward_model_subs.py
+++ wobbles/workflow/forward_model_subs.py
@@ -29,9 +29,8 @@ def VLA_simulation_phasespaceKDE(path_to_file):
     kde = gaussian_kde(vla_subhalo_phase_space, bw_method=0.1)
     return kde
 
-def sample_galactic_potential():
+def sample_galactic_potential(sigma_norm):
 
-    sigma_norm = np.random.uniform(0.7, 1.3)
     sigma_scale = 1.
 
     mwpot = [PowerSphericalPotentialwCutoff(normalize=sigma_norm * 0.05, alpha=1.8,
@@ -39,17 +38,7 @@ def sample_galactic_potential():
             MiyamotoNagaiPotential(a=sigma_scale * 3. / 8., b= sigma_scale * 0.28 / 8., normalize=sigma_norm * 0.6),
             NFWPotential(a=sigma_scale * 2., normalize=sigma_norm * 0.35)]
 
-    return mwpot, sigma_norm
-
-def sample_params():
-
-    nfw_norm = np.random.uniform(0.15, 0.45)
-    disk_norm = np.random.uniform(0.45, 0.75)
-    log_sag_mass_DM = np.random.uniform(9, 11)
-    f_sub = np.random.uniform(0.0, 0.1)
-    vdis = np.random.uniform(15, 35)
-
-    return (nfw_norm, disk_norm, log_sag_mass_DM, f_sub, vdis)
+    return mwpot
 
 def sample_sag_orbit():
 
@@ -75,62 +64,172 @@ def sample_sag_orbit():
 
     return orbit_init_sag
 
-def run(run_index, output_folder_name, VLA_data_path, tabulated_potential):
+def run(run_index, Nreal, output_folder_name, VLA_data_path,
+        tabulated_potential, save_params_list, readout_step=100):
+
+    # 'nfw_norm', 'disk_norm',
+    #  'log_sag_mass_DM', 'sag_mass2light',
+    #  'f_sub',
+    #  'velocity_dispersion', 'compoent_amplitude',
+    #  'orbit_ra', 'orbit_dec', 'orbit_z', 'orbit_pm_ra', 'orbit_pm_dec', 'orbit_vlos'
+
+    init_arrays = True
+    count = 0
+
+    for j in range(0, Nreal):
+        nfw_norm = np.random.uniform(0.15, 0.45)
+        disk_norm = np.random.uniform(0.5, 0.7)
+        log_sag_mass_DM = np.random.uniform(9, 11)
+        sag_mass2light = abs(np.random.normal(50, 5))
+        f_sub = 0.
+        log_slope = -1.9
+        m_host = 1.3 * 10 ** 12
+        velocity_dispersion_1, amp_1 = np.random.uniform(15, 25), 1.
+        velocity_dispersion_2, amp_2 = None, None
+        velocity_dispersion_3, amp_3 = None, None
+        orbit_ra = np.random.normal(250, 10)
+        orbit_dec = np.random.normal(-50, 2)
+        orbit_z = np.random.normal(22, 0.5)
+        orbit_pm_ra, orbit_pm_dec = -3.1, -2.8
+        orbit_vlos = np.random.normal(140, 5)
+        gal_norm = np.random.uniform(0.7, 1.3)
+
+        samples = {'nfw_norm': nfw_norm, 'disk_norm': disk_norm, 'log_sag_mass_DM': log_sag_mass_DM,
+                   'sag_mass2light': sag_mass2light, 'f_sub': f_sub, 'log_slope': log_slope, 'm_host': m_host,
+                   'velocity_dispersion_1': velocity_dispersion_1, 'velocity_dispersion_2': velocity_dispersion_2,
+                  'velocity_dispersion_3': velocity_dispersion_3, 'component_amplitude_1': amp_1,
+                   'component_amplitude_2': amp_2, 'component_amplitude_3': amp_3,
+                   'orbit_ra': orbit_ra, 'orbit_dec': orbit_dec,
+                   'orbit_z': orbit_z, 'orbit_pm_ra': orbit_pm_ra, 'orbit_pm_dec': orbit_pm_dec,
+                   'orbit_vlos': orbit_vlos, 'gal_norm': gal_norm}
+
+        A, vz = _run(VLA_data_path, tabulated_potential, samples)
+        for param in save_params_list:
+            assert param in samples.keys()
+        new_params_sampled = [samples[param] for param in save_params_list]
+        new_params_sampled = np.array(new_params_sampled)
+
+        if init_arrays:
+            init_arrays = False
+            params_sampled = new_params_sampled
+            asymmetry = A
+            mean_vz = vz
+        else:
+            params_sampled = np.vstack((params_sampled, new_params_sampled))
+            asymmetry = np.vstack((asymmetry, A))
+            mean_vz = np.vstack((mean_vz, vz))
+
+        count += 1
+        if count < readout_step:
+            readout = False
+        else:
+            readout = True
+
+        if readout:
+
+            init_arrays = True
+            count = 0
+            with open(output_folder_name + 'asymmetry_' + str(run_index) + '.txt', 'a') as f:
+                string_to_write = ''
+                for row in range(0, asymmetry.shape[0]):
+                    for ai in asymmetry[row,:]:
+                        string_to_write += str(np.round(ai, 5)) + ' '
+                    string_to_write += '\n'
+                f.write(string_to_write)
+
+            with open(output_folder_name + 'meanvz_' + str(run_index) + '.txt', 'a') as f:
+                string_to_write = ''
+                for row in range(0, mean_vz.shape[0]):
+                    for vzi in mean_vz[row, :]:
+                        string_to_write += str(np.round(vzi, 5)) + ' '
+                    string_to_write += '\n'
+                f.write(string_to_write)
+
+            with open(output_folder_name + 'params_' + str(run_index) + '.txt', 'a') as f:
+                string_to_write = ''
+                for row in range(0, params_sampled.shape[0]):
+                    for param_val in params_sampled[row, :]:
+                        string_to_write += str(np.round(param_val, 5)) + ' '
+                    string_to_write += '\n'
+                f.write(string_to_write)
+
+def _run(VLA_data_path, tabulated_potential, samples):
+
     # f is the mass fraction contained in halos between 10^6 and 10^10, CDM prediction is a few percent
 
-    params_sampled = sample_params()
-    [nfw_norm, disk_norm, log_sag_mass_DM, f_sub, velocity_dispersion] = params_sampled
+    keywords = ['nfw_norm', 'disk_norm',
+                'log_sag_mass_DM', 'sag_mass2light',
+                'f_sub', 'log_slope', 'm_host',
+                'velocity_dispersion_1', 'velocity_dispersion_2', 'velocity_dispersion_3',
+                'component_amplitude_1', 'component_amplitude_2', 'component_amplitude_3',
+                 'orbit_ra', 'orbit_dec', 'orbit_z', 'orbit_pm_ra', 'orbit_pm_dec', 'orbit_vlos',
+                'gal_norm']
 
-    potential_local = tabulated_potential.evaluate(nfw_norm, disk_norm)
+    for kw in keywords:
+        assert kw in samples.keys(), 'did not find '+str(kw)
 
-    galactic_potential, gal_norm = sample_galactic_potential()
-    params_sampled = np.append(params_sampled, gal_norm)
-    potential_global = PotentialExtension(galactic_potential, 2, 120, 100, compute_action_angle=False)
+    velocity_dispersion = [samples['velocity_dispersion_1']]
+    component_amplitude = [samples['component_amplitude_1']]
+    if samples['velocity_dispersion_2'] is not None:
+        velocity_dispersion.append(samples['velocity_dispersion_2'])
+        component_amplitude.append(samples['component_amplitude_2'])
+        if samples['velocity_dispersion_3'] is not None:
+            velocity_dispersion.append(samples['velocity_dispersion_3'])
+            component_amplitude.append(samples['component_amplitude_3'])
+    assert len(velocity_dispersion) == len(component_amplitude)
+
+    potential_local = tabulated_potential.evaluate(samples['nfw_norm'],
+                                                   samples['disk_norm'])
+
+    galactic_potential = sample_galactic_potential(samples['gal_norm'])
+
+    potential_global = PotentialExtension(galactic_potential, 2, 120, 100,
+                                          compute_action_angle=False)
 
     kde = VLA_simulation_phasespaceKDE(VLA_data_path)
 
-    m_host = 1.3 * 10 ** 12
-    mlow, mhigh = 5 * 10 ** 6, 5 * 10 ** 9
-    log_slope = -1.9
-    N_halos = normalization(f_sub, log_slope, m_host, mlow, mhigh)
-    sample_orbits_0 = generate_sample_orbits_kde(N_halos, kde, galactic_potential, time_Gyr)
-    print('generated ' + str(N_halos) + ' halos... ')
-    #######################################
+    mlow, mhigh = 5 * 10 ** 6, 10 ** 9
+    N_halos = normalization(samples['f_sub'], samples['log_slope'], samples['m_host'], mlow, mhigh)
 
+    ####################################### Generate subhalo orbits #######################################
+    sample_orbits_0 = generate_sample_orbits_kde(N_halos, kde, galactic_potential, time_Gyr)
+    #print('generated ' + str(N_halos) + ' halos... ')
     rs_host, r_core = 30, 30.
     args, func = (rs_host, r_core), core_nfw_pdf
     inds_keep = filter_orbits_NFW(sample_orbits_0, time_Gyr, func, args)
-    print('removed ' + str(N_halos - len(inds_keep)) + ' halos from r < ' + str(r_core) + ' kpc...')
     sample_orbits_1 = [sample_orbits_0[idx] for idx in inds_keep]
-    # get orbits that passed within dr_max of the sun in the last t_orbit years
     dr_max = 8  # kpc
     nearby_orbits_1_inds, _ = passed_near_solar_neighorhood(sample_orbits_1, time_Gyr, potential_global, R_solar=8,
                                                      dr_max=dr_max, pass_through_disk_limit=3, tdep=True)
     nearby_orbits_1 = [sample_orbits_0[idx] for idx in nearby_orbits_1_inds]
     n_nearby_1 = len(nearby_orbits_1)
-    print('kept ' + str(n_nearby_1) + ' halos... ')
-    #######################################
+    #print('kept ' + str(n_nearby_1) + ' halos... ')
+    ############################################################################################################
 
-    halo_masses_1 = sample_mass_function(n_nearby_1, log_slope, mlow, mhigh)
+    ####################################### Set subhalo properties #######################################
+    halo_masses_1 = sample_mass_function(n_nearby_1, samples['log_slope'], mlow, mhigh)
     halo_concentrations_1 = sample_concentration(halo_masses_1)
-    #halo_concentrations_1 = sample_hernquist_a(np.array(halo_masses_1))
-
     halo_potentials_1 = []
     for m, c in zip(halo_masses_1, halo_concentrations_1):
         halo_potentials_1.append(NFWPotential(mvir=m / 10 ** 12, conc=c))
-        #halo_potentials_1.append(HernquistPotential(amp=2 * m * apu.solMass, a=c * apu.kpc))
+    #####################################################################################################
 
-    orbit_init_sag = sample_sag_orbit()
+    ####################################### Integrate orbit of Sag. #################################
+    orbit_init_sag = [samples['orbit_ra'] * apu.deg, samples['orbit_dec'] * apu.deg,
+                      samples['orbit_z'] * apu.kpc,
+                      samples['orbit_pm_ra'] * apu.mas / apu.yr, samples['orbit_pm_dec'] * apu.mas / apu.yr,
+                      samples['orbit_vlos'] * apu.km / apu.s]
     sag_orbit_phsical_off = integrate_orbit(orbit_init_sag, galactic_potential, time_Gyr)
     sag_orbit = [sag_orbit_phsical_off]
-
     halo_orbit_list_physical_off_1 = []
     for orb in nearby_orbits_1:
         orb.turn_physical_off()
         halo_orbit_list_physical_off_1.append(orb)
+    #######################################################################################################
 
-    m_sag_dm = 10 ** log_sag_mass_DM
-    m_sag_stellar = m_sag_dm/50
+    ####################################### Set Sag. properties ########################################
+    m_sag_dm = 10 ** samples['log_sag_mass_DM']
+    m_sag_stellar = m_sag_dm/samples['sag_mass2light']
 
     a_sag = 3 * (m_sag_dm/10**10) ** 1./3
     a_stellar = 1.5 * (m_sag_stellar/10**9) ** 1./3
@@ -139,42 +238,33 @@ def run(run_index, output_folder_name, VLA_data_path, tabulated_potential):
     sag_potential_2 = galpy.potential.HernquistPotential(amp=m_sag_stellar * apu.M_sun, a=a_stellar * apu.kpc)
     sag_potential = [sag_potential_1 + sag_potential_2]
     galpy.potential.turn_physical_off(sag_potential)
+    #######################################################################################################
 
+    ############################# Compute distribution function ############################################
     disc = Disc(potential_local, potential_global)
     time_internal_units = sag_orbit_phsical_off.time()
 
     perturber_orbits = sag_orbit + halo_orbit_list_physical_off_1
     perturber_potentials = sag_potential + halo_potentials_1
     dF, delta_J, force = compute_df(disc, time_internal_units,
-                                    perturber_orbits, perturber_potentials, velocity_dispersion, verbose=False)
+                                    perturber_orbits, perturber_potentials, velocity_dispersion,
+                                    component_amplitude, verbose=False)
 
-    run_index = int(run_index)
     asymmetry, mean_vz = dF.A, dF.mean_v_relative
-    print(output_folder_name + 'asymmetry_' + str(run_index) + '.txt')
-    with open(output_folder_name + 'asymmetry_' + str(run_index) + '.txt', 'a') as f:
-        string_to_write = ''
-        for ai in asymmetry:
-            string_to_write += str(np.round(ai, 5)) + ' '
-        string_to_write += '\n'
-        f.write(string_to_write)
+    return asymmetry, mean_vz
 
-    with open(output_folder_name + 'meanvz_' + str(run_index) + '.txt', 'a') as f:
-        string_to_write = ''
-        for vzi in mean_vz:
-            string_to_write += str(np.round(vzi, 5)) + ' '
-        string_to_write += '\n'
-        f.write(string_to_write)
-
-    with open(output_folder_name + 'params_' + str(run_index) + '.txt', 'a') as f:
-        string_to_write = ''
-        for param_val in params_sampled:
-            string_to_write += str(np.round(param_val, 5)) + ' '
-        string_to_write += '\n'
-        f.write(string_to_write)
 #
-# Nreal = 200
+# Nreal = 6
 # VLA_data_path = os.getenv('HOME') + '/Code/external/wobbles/wobbles/workflow/'
-# output_folder = './output/forward_model_samples/'
+# output_folder = './output/forward_model_samples_new/'
+# f = open('./saved_potentials/tabulated_MWpot', 'rb')
+# tabulated_potential = pickle.load(f)
+# f.close()
+#
+# save_params_list = ['nfw_norm', 'disk_norm', 'log_sag_mass_DM', 'velocity_dispersion_1']
+#
 # for iter in range(Nreal):
-#     print(str(Nreal - iter) + ' remaining...')
-#     run(1, output_folder, VLA_data_path)
+#     #print(str(Nreal - iter) + ' remaining...')
+#
+#     run(1, Nreal, output_folder, VLA_data_path,
+#         tabulated_potential, save_params_list, readout_step=2)
